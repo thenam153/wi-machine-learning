9f76920a9e03112e6d9d78e888406bb72887f95b
BuiHoang <bdhoang.ht@gmail.com>
2019-08-14 17:47:28 +0700

    hoangbd update

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 const gulp = require('gulp');
 const downloadFile = require("gulp-download");
 
-gulp.task('pre', function() {
-	// let stream = downloadFile('https://www.dropbox.com/scl/fi/dyrdwclgcnqimzbd8wtj8/Untitled.gsheet?dl=0&rlkey=iu3z2opnenevuamg30ez9kz3i#gid=0').pipe(gulp.dest("./")); 
+gulp.task('pre', function (cb) {
+	let stream = downloadFile('http://file.i2g.cloud/wi-xlsx/wi-uservice.xlsx').pipe(gulp.dest("./"));
 	var wiMl = require('./excel-to-json.js');
-	// stream.on('end', function() {
-	// 	let inputFile = './wi-uservice.xlsx';
-	// 	let outputFile = './wi-uservice.json';
-	// 	let configFile = './src/config/config.js';
-	// 	wiMl.sheetToJson(inputFile, outputFile, configFile);
-	// });
-	let inputFile = './wi-uservice.xlsx';
-	let outputFile = './src/model-selection/model/wi-uservice.json';
-	let configFile = './src/config/config.js';
-	return wiMl.sheetToJson(inputFile, outputFile, configFile);
+	stream.on('end', function () {
+		let inputFile = './wi-uservice.xlsx';
+		let outputFile = './src/model-selection/model/wi-uservice.json';
+		let configFile = './src/config/config.js';
+		wiMl.sheetToJson(inputFile, outputFile, configFile);
+		cb();
+	});
+	stream.on('error', function (e) {
+		console.log("Loi ", e)
+	});
+	// let inputFile = './wi-uservice.xlsx';
+	// let outputFile = './src/model-selection/model/wi-uservice.json';
+	// let configFile = './src/config/config.js';
+	// return wiMl.sheetToJson(inputFile, outputFile, configFile);
 });
