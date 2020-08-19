b37e8213f94c117d53b7eba64a4f8f9081ee9683
Quang LN <quangln2810@gmail.com>
2020-08-19 12:00:44 +0700

    update build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: "development",
+	mode: process.env.NODE_ENV || "development",
 	entry: {
 		main: "./src/main.js",
 	},
