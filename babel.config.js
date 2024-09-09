78ff017b48a8baffb6f57ab71594047c272d37e9
Quang LN <quangln2810@gmail.com>
2024-09-09 21:49:32 +0700

    fix build

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-      ['@babel/plugin-proposal-nullish-coalescing-operator'],
-      ['@babel/plugin-proposal-optional-chaining']
+      ['@babel/plugin-transform-nullish-coalescing-operator'],
+      ['@babel/plugin-transform-optional-chaining']
   ]
 }
